f3d4566e524faca22e609b6bf6a704c71670d24d
JEriksson <jani.eriksson@3stepit.com>
2016-06-09 17:02:39 +0300

    navbar lisätty

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,10 +1,27 @@
 import React, { PropTypes } from 'react'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import { Link } from 'react-router'
 
 class MainLayout extends React.Component {
   render () {
     return (
-      <div className="container">
-        {this.props.children}
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">EM-kisaveikkaus</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <li><Link to="/users">Veikkaajat</Link></li>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <div className="container">
+          {this.props.children}
+        </div>
       </div>
     )
   }
